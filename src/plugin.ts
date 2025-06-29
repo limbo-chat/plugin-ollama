28963bb33825f98fd0ee14a64d2ea320a979a79c
Wes Lowe <wesley.lowe@yahoo.com>
2025-06-29 14:00:15 -0500

    updated to use new rollup plugin limbo, added dotenv, added pnpm workspace config

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,16 +1,41 @@
-import * as limbo from "limbo";
+import * as limbo from "@limbo/api";
 import ollama from "ollama/browser";
+import {
+	convertMessagesToOpenAICompatible,
+	convertToolsToOpenAICompatible,
+	FetchAdapter,
+	OpenAICompatibleClient,
+	streamOpenAICompatibleChatCompletion,
+} from "@limbo/openai-utils";
+
+/*
+  Considerations:
+
+  If the user changes the host after the plugin is activated, the plugin will not refetch the models again.
+  Also, generateText should always access the most recent settings (such as the ollama host)
+
+  *There may need to be a way to watch settings in the plugin API*
+*/
 
 export default {
 	onActivate: async () => {
-		// limbo.settings.register({
-		// 	id: "host",
-		// 	type: "text",
-		// 	label: "Ollama host",
-		// 	description: "The Ollama host URL",
-		// 	defaultValue: "http://localhost:11434",
-		// 	placeholder: "http://localhost:11434",
-		// });
+		limbo.commands.register({
+			id: "refresh-models",
+			name: "Refresh Models",
+			execute: async () => {
+				console.log("refreshing ollama models");
+			},
+		});
+		// temp, not doing anything with the settings yet
+
+		limbo.settings.register({
+			id: "host",
+			type: "text",
+			label: "Ollama host",
+			description: "The Ollama host URL",
+			defaultValue: "http://localhost:11434",
+			placeholder: "http://localhost:11434",
+		});
 
 		// --- llms ---
 
@@ -21,18 +46,46 @@ export default {
 				id: model.name,
 				name: model.name,
 				description: `Ollama model: ${model.name}`,
-				generateText: async ({ promptBuilder, onChunk }) => {
-					const messages = promptBuilder.getMessages();
-
-					const response = await ollama.chat({
-						model: model.name,
-						stream: true,
-						messages,
+				capabilities: [],
+				chat: async ({ messages, tools, onText, abortSignal }) => {
+					const client = new OpenAICompatibleClient({
+						adapter: new FetchAdapter(),
+						baseUrl: "http://localhost:11434/v1",
 					});
 
-					for await (const chunk of response) {
-						onChunk(chunk.message.content);
+					const openaiTools = convertToolsToOpenAICompatible(tools);
+					const openaiMessages = convertMessagesToOpenAICompatible(messages);
+
+					console.log("openaiTools", openaiTools);
+
+					const stream = streamOpenAICompatibleChatCompletion(client, {
+						model: model.name,
+						tools: openaiTools,
+						messages: openaiMessages,
+						abortSignal,
+					});
+
+					for await (const chunk of stream) {
+						const text = chunk.content;
+
+						console.log("chunk", chunk);
+
+						if (text) {
+							onText(text);
+						}
 					}
+
+					// const response = await ollama.chat({
+					// 	model: model.name,
+					// 	stream: true,
+					// 	// tools: adaptedTools,
+					// 	messages: [
+					// 		{
+					// 			role: "assistant",
+					// 			content: "",
+					// 		},
+					// 	],
+					// });
 				},
 			});
 		}
